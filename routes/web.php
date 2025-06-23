0ff9c38e06a823b0883b6192bf3179f236946a2b
Meo-ICAR <piergiuseppe.meo@icar.cnr.it>
2025-06-23 14:44:20 +0200

    logout

--- routes/web.php
+++ routes/web.php
@@ -61,7 +61,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::get('/home', function () {
         return view('dashboard');
-    })->name('home');
+    })->name('dashboard');
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
